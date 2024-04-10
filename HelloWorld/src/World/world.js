9ea6986279787781c6caf149525246ea5f3a7dc4
frekk dekk <99095385+ffreakyddeaky@users.noreply.github.com>
2024-04-09 19:14:57 -0700

    Refactored hello cube render following the World App tutorial.

---
+++ HelloWorld/src/World/world.js
@@ -0,0 +1,36 @@
+import { createCamera } from './components/camera.js';
+import { createCube } from './components/cube.js';
+import { createLights } from './components/lights.js';
+import { createScene } from './components/scene.js';
+
+import { createRenderer } from './systems/renderer.js';
+import { Resizer } from './systems/resizer.js';
+
+// These variables are module-scoped: we cannot access them
+// from outside the module
+let camera;
+let renderer;
+let scene;
+
+class World {
+    constructor(container) {
+        camera = createCamera();
+        scene = createScene();
+        renderer = createRenderer();
+        container.append(renderer.domElement);
+
+        const cube = createCube();
+        const light = createLights();
+
+        scene.add(cube, light);
+
+        const resizer = new Resizer(container, camera, renderer);
+    }
+
+    render() {
+        // draw a single frame
+        renderer.render(scene, camera);
+    }
+}
+
+export { World };
--- HelloWorld/src/routes/assignment1/+page.svelte
+++ HelloWorld/src/routes/assignment1/+page.svelte
@@ -1,67 +1,20 @@
 <script>
-    import { onMount } from "svelte";
-    import {
-        BoxGeometry,
-        Color,
-        Mesh,
-        MeshBasicMaterial,
-        PerspectiveCamera,
-        Scene,
-        WebGLRenderer,
-    } from "three";
+    import { onMount } from 'svelte';
+    import { World } from '../../World/world.js';
 
     onMount(() => {
-        // Get a reference to the container element that will hold our scene
-        const container = document.querySelector("#scene_1");
 
-        // create a Scene
-        const scene = new Scene();
+        const container = document.querySelector('#scene_1');
 
-        // Set the background color
-        scene.background = new Color("skyblue");
+        // 1. Create an instance of the World app
+        const world = new World(container);
 
-        // create the renderer
-        const renderer = new WebGLRenderer();
-
-        // Create a camera
-        const fov = 35; // AKA Field of View
-        const aspect = container.clientWidth / container.clientHeight;
-        const near = 0.1; // the near clipping plane
-        const far = 100; // the far clipping plane
-
-        const camera = new PerspectiveCamera(fov, aspect, near, far);
-
-        // every object is initially created at ( 0, 0, 0 )
-        // move the camera back so we can view the scene
-        camera.position.set(3, 1, 10);
-
-        // create a geometry
-        const geometry = new BoxGeometry(2, 2, 2);
-
-        // create a default (white) Basic material
-        const material = new MeshBasicMaterial();
-
-        // create a Mesh containing the geometry and material
-        const cube = new Mesh(geometry, material);
-
-        // add the mesh to the scene
-        scene.add(cube);
-
-        // next, set the renderer to the same size as our container element
-        renderer.setSize(container.clientWidth, container.clientHeight);
-
-        // finally, set the pixel ratio so that our scene will look good on HiDPI displays
-        renderer.setPixelRatio(window.devicePixelRatio);
-
-        // add the automatically created <canvas> element to the page
-        container.append(renderer.domElement);
-
-        // render, or 'create a still image', of the scene
-        renderer.render(scene, camera);
+        // 2. Render the scene
+        world.render();
 
     });
-
 </script>
+
 
 <p class="display-5 text-center">Hello Cube</p>
 
